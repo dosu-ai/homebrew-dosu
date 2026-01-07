5c5e73540bfc6238f6052707a096d64dd15a8eb8
dhij <davidhwang.ij@gmail.com>
2026-01-07 18:19:17 -0500

    update brew formula version to v0.1.0-alpha

--- Formula/dosu.rb
+++ Formula/dosu.rb
@@ -4,28 +4,27 @@
 class Dosu < Formula
   desc "CLI tool for Dosu"
   homepage "https://github.com/dosu-ai/dosu-cli"
-  version "0.0.1-alpha"
-  license "MIT"
+  version "0.1.0-alpha"
 
   on_macos do
     on_intel do
       url "https://github.com/dosu-ai/dosu-cli/releases/download/v#{version}/dosu_Darwin_x86_64.tar.gz"
-      sha256 "60cf2823b9b06e79e7e62f154cfd9b7078ecaec8a0ee17a58b1e91229c244fd5"
+      sha256 "fc94a8a1f9d0f7301bf65258e1ec74b75f802db0920fbd5dc70516796c3dec82"
     end
     on_arm do
       url "https://github.com/dosu-ai/dosu-cli/releases/download/v#{version}/dosu_Darwin_arm64.tar.gz"
-      sha256 "6a14a89d2cf30068ecb6e0e0c00f37d92dcf0fad7193a18fd7bb2cf24ec4c251"
+      sha256 "7d50eae0bbb69bfb09cd1ed1f1ce8d35c97d8136fd935cfdb2504e4c0394c86e"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/dosu-ai/dosu-cli/releases/download/v#{version}/dosu_Linux_x86_64.tar.gz"
-      sha256 "7acfcbe7061509ccaef1ac6e2438380e8bb0d27fbcd7160161224108a76ea170"
+      sha256 "4e174bb3259386a8f1a7c88366e77c0682a502c07b0e3f1dd934908601b6b12b"
     end
     on_arm do
       url "https://github.com/dosu-ai/dosu-cli/releases/download/v#{version}/dosu_Linux_arm64.tar.gz"
-      sha256 "e51baf4cec15499e7c1379e7cb3f0d304178782a43ec1181185239333faa027b"
+      sha256 "9a5031c3e77f6f39c5e9c701ab463e97946a4442de852f1a669bdc6f62143e43"
     end
   end
 
